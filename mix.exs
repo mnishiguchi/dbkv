068d5cedad54c8536d8fcfed4d80edcf51bf02bf
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-06-29 21:51:22 -0400

    Add GH Actions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DubDB.MixProject do
     [
       app: :dubdb,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
